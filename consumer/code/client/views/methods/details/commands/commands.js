a9c12ad5202822e91c6426557c980ecbc5233928
ebastidas <ebastidas3@gmail.com>
2015-03-02 17:59:29 +0100

    Add "Run" button to a method that calls all the command listed (via the wsdl of the device). Use a third party SiLA device Simulator if a real device is not available.

--- consumer/code/client/views/methods/details/commands/commands.js
+++ consumer/code/client/views/methods/details/commands/commands.js
@@ -31,7 +31,7 @@ var MethodsDetailsCommandsViewItems = function(cursor) {
 	} else {
 		searchString = searchString.replace(".", "\\.");
 		var regEx = new RegExp(searchString, "i");
-		var searchFields = ["commandId", "commandName", "command_parameters", "status"];
+		var searchFields = ["commandId", "requestId", "commandName", "command_parameters", "status"];
 		filtered = _.filter(raw, function(item) {
 			var match = false;
 			_.each(searchFields, function(field) {
@@ -61,14 +61,72 @@ var MethodsDetailsCommandsViewItems = function(cursor) {
 
 var MethodsDetailsCommandsViewExport = function(cursor, fileType) {
 	var data = MethodsDetailsCommandsViewItems(cursor);
-	var exportFields = ["commandId", "commandName", "command_parameters", "status"];
+	var exportFields = ["commandId", "requestId", "commandName", "command_parameters", "status"];
 
 	var str = convertArrayOfObjects(data, exportFields, fileType);
 
 	var filename = "export." + fileType;
 
 	downloadLocalResource(str, filename, "application/octet-stream");
-}
+};
+
+
+var MethodsDetailsCommandsRun = function(cursor, methodId) {
+
+	var commands = MethodsDetailsCommandsViewItems(cursor);
+	//console.log(commands[0]);
+
+	//TODO:
+	//1. queue all the commands -> set Status to 'queued' to all commands. Issue: Find better solution for async commands, this will stop the client until all the commands finish 
+	//2. call each command sequentially: connectDeviceSoap
+
+	//TODO: add lock functionality
+
+	var meth = Methods.findOne({_id : methodId});
+	var dev = Devices.findOne({_id : meth.deviceId});
+	var url = dev.url;
+
+	_.each(commands, function(comm){
+
+		MethodCommands.update({ _id: comm._id }, { "$set": {"status":"running..."}});		
+
+		var command = comm.commandName;
+		if(comm.command_parameters){
+			var argsString = '{"requestId" : "' + comm.requestId + '", ' + comm.command_parameters + '}'; // TODO: add dynamic parameters for all the commands
+		
+		}else{
+			var argsString = '{"requestId" : "' + comm.requestId + '"}'; // TODO: add dynamic parameters for all the commands				
+		}
+		
+		var args = JSON.parse(argsString);
+
+		Meteor.call('connectDeviceSoap', url, command, args, function (error,response) {
+	  		// identify the error
+	  		if (!error) {
+				//TODO: check if device is locked and can't get the information.
+				var newStatus;
+				switch(command) {
+				    case "GetStatus":
+					newStatus = "(" + response.GetStatusResult.returnCode + ") " + (response.GetStatusResult.returnCode==1?"success":response.GetStatusResult.message);
+					break;
+				    case "GetDeviceIdentification":
+					
+					break;
+				    default: //TODO: all the async commands go here (get dynamic response)
+					newStatus = "(" + response.ResetResult.returnCode + ") " + response.ResetResult.message;					
+				}				
+				MethodCommands.update({ _id: comm._id }, { "$set": {"status": newStatus}});
+			}
+			else
+			{
+		    		// show a nice error message
+		    		console.log("error soap");
+				MethodCommands.update({ _id: comm._id }, { "$set": {"status":"connection error"}});
+			}
+		});
+	});
+};
+
 
 
 Template.MethodsDetailsCommandsView.rendered = function() {
@@ -153,6 +211,12 @@ Template.MethodsDetailsCommandsView.events({
 	"click #dataview-export-json": function(e, t) {
 		e.preventDefault();
 		MethodsDetailsCommandsViewExport(this.method_commands, "json");
+	},
+
+	"click #dataview-run-button": function(e, t) {
+		e.preventDefault();
+		MethodsDetailsCommandsRun(this.method_commands, this.params.methodId);
+		//Router.go("methods.details.insert", {methodId: this.params.methodId});
 	}
 
 	
