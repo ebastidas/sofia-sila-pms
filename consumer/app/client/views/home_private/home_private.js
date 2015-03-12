b8d78ebb79e9e6596d49398497d52c4801280d3f
ebastidas <ebastidas3@gmail.com>
2015-03-12 13:44:10 +0100

    home page changes

--- consumer/app/client/views/home_private/home_private.js
+++ consumer/app/client/views/home_private/home_private.js
@@ -9,3 +9,19 @@ Template.HomePrivate.events({
 Template.HomePrivate.helpers({
 	
 });
+
+Template.HomePrivateHomeJumbotron.rendered = function() {
+	
+};
+
+Template.HomePrivateHomeJumbotron.events({
+	"click #jumbotron-button": function(e, t) {
+		e.preventDefault();
+		Router.go("devices", {});
+	}
+	
+});
+
+Template.HomePrivateHomeJumbotron.helpers({
+	
+});
