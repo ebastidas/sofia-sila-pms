493fedacc76bb40ef742b34992dc21040b54e8c4
ebastidas <ebastidas3@gmail.com>
2015-03-20 13:37:46 +0100

    Updated License

--- consumer/app/lib/string_utils.js
+++ consumer/app/lib/string_utils.js
@@ -1,10 +1,30 @@
-//Copyright (c) 2015 wega Informatik AG | Erick Bastidas
-//================================
-//Company: wega Informatik AG
-//Website: http://www.wega-it.com
-//Author: Erick Bastidas
-//Email: ebastidas3@gmail.com
-//================================
+//=========================================================================
+// Copyright (c) 2015 wega Informatik AG | Erick Bastidas
+//
+// This file is part of SOFIA.
+//
+// SOFIA is free software: you can redistribute it and/or modify it under 
+// the terms of the GNU Lesser General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or (at your 
+// option) any later version.
+//
+// SOFIA is distributed in the hope that it will be useful, but WITHOUT 
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License 
+// along with SOFIA. If not, see <http://www.gnu.org/licenses/>.
+//
+//======================================================
+// Copyright details
+//======================================================
+//   Company: wega Informatik AG
+//   Address: Aeschengraben 20, 4051 Basel, Switzerland
+//   Website: http://www.wega-it.com
+//   Author: Erick Bastidas
+//   Email: ebastidas3@gmail.com
+//=========================================================================
 
 
 this.escapeRegEx = function (string) {
--- consumer/event-receiver/app.js
+++ consumer/event-receiver/app.js
@@ -1,15 +1,35 @@
-//Copyright (c) 2015 wega Informatik AG | Erick Bastidas
-//================================
-//Company: wega Informatik AG
-//Website: http://www.wega-it.com
-//Author: Erick Bastidas
-//Email: ebastidas3@gmail.com
-//================================
+//=========================================================================
+// Copyright (c) 2015 wega Informatik AG | Erick Bastidas
+//
+// This file is part of SOFIA.
+//
+// SOFIA is free software: you can redistribute it and/or modify it under 
+// the terms of the GNU Lesser General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or (at your 
+// option) any later version.
+//
+// SOFIA is distributed in the hope that it will be useful, but WITHOUT 
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+// License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License 
+// along with SOFIA. If not, see <http://www.gnu.org/licenses/>.
+//
+//======================================================
+// Copyright details
+//======================================================
+//   Company: wega Informatik AG
+//   Address: Aeschengraben 20, 4051 Basel, Switzerland
+//   Website: http://www.wega-it.com
+//   Author: Erick Bastidas
+//   Email: ebastidas3@gmail.com
+//=========================================================================
 
 
   //////browser debugger in http://0.0.0.0:50500
   //var nomo = require('node-monkey').start({host:'0.0.0.0', port: 8081});
-  ///////////
+  /////////////////////////////////////////////
   
   // Connection URL
   var url = 'mongodb://127.0.0.1:3001/meteor'; //DEPLOY: get relative IP
