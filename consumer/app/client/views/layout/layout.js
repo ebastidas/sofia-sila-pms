c86d367b75512ab905b540adcc622320fe8b6297
ebastidas <ebastidas3@gmail.com>
2015-04-13 14:00:50 +0200

    updated license

--- consumer/app/client/views/layout/layout.js
+++ consumer/app/client/views/layout/layout.js
@@ -4,16 +4,16 @@
 // This file is part of SOFIA.
 //
 // SOFIA is free software: you can redistribute it and/or modify it under 
-// the terms of the GNU Lesser General Public License as published by the 
+// the terms of the GNU General Public License as published by the 
 // Free Software Foundation, either version 3 of the License, or (at your 
 // option) any later version.
 //
 // SOFIA is distributed in the hope that it will be useful, but WITHOUT 
 // ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public 
 // License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public License 
+// You should have received a copy of the GNU General Public License 
 // along with SOFIA. If not, see <http://www.gnu.org/licenses/>.
 //
 //======================================================
@@ -110,6 +110,8 @@ Template.PrivateLayoutLeftMenu.rendered = function() {
 		$(this).parent().find(".collapse").each(function() {
 			if($(this).find("li.active").length) {
 				$(this).addClass("in");
+
+				//$(this).addClass("collapse");
 			}
 		});
 	});
@@ -133,6 +135,7 @@ Template.PrivateLayoutRightMenu.rendered = function() {
 		$(this).parent().find(".collapse").each(function() {
 			if($(this).find("li.active").length) {
 				$(this).addClass("in");
+				//$(this).addClass("collapse");
 			}
 		});
 	});
