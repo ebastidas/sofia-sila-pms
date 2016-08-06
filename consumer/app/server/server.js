73e1b5afdb76219a342c22ce543d4dbc5099efe9
ebastidas <ebastidas3@gmail.com>
2016-08-06 02:35:52 -0500

    Update server.js

--- consumer/app/server/server.js
+++ consumer/app/server/server.js
@@ -34,8 +34,10 @@ Meteor.startup(function() {
 			process.env[variableName] = Meteor.settings.env[variableName];
 		}
 	}
-
-	//Add initial documents to the database if empty
+	
+	//
+	// Add initial documents (SiLA Command Command Set) to the database if empty
+	//
 	if(!CommonCommandSet.find().count()) {
 		CommonCommandSet.insert({"deviceClassVersion":0.97,"name":"Abort","parameters":"-","returnValues":"","0":"M","1":"M","2":"M","3":"M","4":"M","5":"M","6":"M","7":"M","8":"M","9":"M","10":"M","11":"M","12":"M","13":"M","14":"M","15":"M","16":"M","17":"M","18":"M","19":"M","20":"M","21":"M","22":"M","23":"M","24":"M","25":"M","26":"M","27":"M","28":"M","29":"M","30":"M","31":"M","901":"M","902":"M","903":"M","1000":"M"});
 		CommonCommandSet.insert({"deviceClassVersion":0.97,"name":"DoContinue","parameters":"-","returnValues":"","0":"M","1":"M","2":"M","3":"M","4":"M","5":"M","6":"M","7":"M","8":"M","9":"M","10":"M","11":"M","12":"M","13":"M","14":"M","15":"M","16":"M","17":"M","18":"M","19":"M","20":"M","21":"M","22":"M","23":"M","24":"M","25":"M","26":"M","27":"M","28":"M","29":"M","30":"M","31":"M","901":"M","902":"M","903":"M","1000":"M"});
@@ -140,6 +142,11 @@ Meteor.startup(function() {
 
 });
 
+
+//
+// INITIALIZE SERVER SIDE METHODS
+//
+
 Meteor.methods({
 	  "connectDeviceSoap": function(url, operation, args) {
 
@@ -233,6 +240,7 @@ Meteor.methods({
 
 
 });
+
 
 Accounts.onCreateUser(function (options, user) {
 	user.roles = [];
